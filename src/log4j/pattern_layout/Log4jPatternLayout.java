726cabfe66bdaa46f04ec15d4218c5d873fc6d45
Marco Rieger <Marco.Rieger@gmx.de>
2019-05-20 19:54:52 +0200

    Converted PatternLayout into Thread

--- src/log4j/pattern_layout/Log4jPatternLayout.java
+++ src/log4j/pattern_layout/Log4jPatternLayout.java
@@ -1,16 +1,19 @@
 package log4j.pattern_layout;
 
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.DailyRollingFileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
-public class Main {
+public class Log4jPatternLayout extends Thread {
 	private static Logger logger = Logger.getRootLogger();
 
 	public static void main(String[] args) {
 		try {
 			PatternLayout layout = new PatternLayout("%d %l: %-5p [%t]: %m (%r)%n");
+			ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+			logger.addAppender(consoleAppender);
 			DailyRollingFileAppender fileAppender = new DailyRollingFileAppender(layout, "logs/logs.log",
 					"'.'yyyy-MM-dd_HH-mm");
 			logger.addAppender(fileAppender);
@@ -18,10 +21,23 @@ public class Main {
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
-		logger.debug("Debug-Meldung");
-		logger.info("Info-Meldung");
-		logger.warn("Warn-Meldung");
-		logger.error("Error-Meldung");
-		logger.fatal("Fatal-Meldung");
+		new Log4jPatternLayout().start();
+		new Log4jPatternLayout().start();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			logger.debug("Debug-Meldung");
+			logger.info("Info-Meldung");
+			logger.warn("Warn-Meldung");
+			logger.error("Error-Meldung");
+			logger.fatal("Fatal-Meldung");
+		}
 	}
 }
