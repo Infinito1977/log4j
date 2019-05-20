7bf7c7768a8ab51396d10a7f55a60c04e1dcba58
Marco Rieger <Marco.Rieger@gmx.de>
2019-05-20 20:03:24 +0200

    Converted Properties version into a Thread

---
+++ src/log4j/properties/Log4jPatternLayout.java
@@ -0,0 +1,30 @@
+package log4j.properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jPatternLayout extends Thread {
+	private static Logger logger = Logger.getRootLogger();
+
+	public static void main(String[] args) {
+		// Use custom properties file and reload it every 5th second
+		PropertyConfigurator.configureAndWatch("log4j-custom.properties", 5 * 1000);
+		new Log4jPatternLayout().start();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			logger.debug("Debug-Meldung");
+			logger.info("Info-Meldung");
+			logger.warn("Warn-Meldung");
+			logger.error("Error-Meldung");
+			logger.fatal("Fatal-Meldung");
+		}
+	}
+}
