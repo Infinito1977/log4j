788b725831e0843d96642e0a0d5ad367c3100db6
Marco Rieger <Marco.Rieger@gmx.de>
2019-06-02 21:11:11 +0200

    Example using default configuration file

---
+++ src/log4j/client/properties_file/DefaultPropertiesFile.java
@@ -0,0 +1,28 @@
+package log4j.client.properties_file;
+
+import org.apache.log4j.Logger;
+
+public class DefaultPropertiesFile extends Thread {
+	private static Logger logger = Logger.getRootLogger();
+
+	public static void main(String[] args) {
+		// Load default properties file src/log4j.properties
+		new DefaultPropertiesFile().start();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			logger.debug("Debug-Meldung");
+			logger.info("Info-Meldung");
+			logger.warn("Warn-Meldung");
+			logger.error("Error-Meldung");
+			logger.fatal("Fatal-Meldung");
+		}
+	}
+}
